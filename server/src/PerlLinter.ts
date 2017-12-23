27db2c7daea1652562c00350b934a9f688089669
Jack Karamanian <karamanian.jack@gmail.com>
2017-12-23 12:33:20 -0600

    Handle "has too many errors" behavior

--- server/src/PerlLinter.ts
+++ server/src/PerlLinter.ts
@@ -3,7 +3,6 @@ import * as _ from 'lodash';
 import { Diagnostic, Range, Position, DiagnosticSeverity } from 'vscode-languageserver';
 
 const LINE_REGEX = /line (\d*)[\.,]/;
-const SPLICE_SIZE = 1000;
 
 interface DocumentProcess {
     [document: string]: childProcess.ChildProcess;
@@ -12,7 +11,12 @@ interface DocumentProcess {
 export class PerlLinter {
     private documentProcesses: DocumentProcess;
 
-    constructor(public perlExecutable: string, public includePaths: string[], public perlOptions: string[], public prependCode: string[]) {
+    constructor(
+        public perlExecutable: string,
+        public includePaths: string[],
+        public perlOptions: string[],
+        public prependCode: string[]
+    ) {
         this.documentProcesses = {};
     }
 
@@ -20,39 +24,63 @@ export class PerlLinter {
         const diagnostics: Diagnostic[] = [];
 
         let process: childProcess.ChildProcess = this.documentProcesses[uri];
+
         if (process) {
             process.kill('SIGINT');
         }
 
-        this.documentProcesses[uri] = process = childProcess.spawn(this.perlExecutable, ['-c', ...this.perlOptions, ...this.includePaths.map(path => '-I' + path)]);
-        
-        process.stdin.write(this.prependCode.join(''));
-        for (const i of _.range(0, _.ceil(text.length / SPLICE_SIZE))) {
-            const textSlice = text.slice(SPLICE_SIZE * i, (SPLICE_SIZE * i) + SPLICE_SIZE);
-            process.stdin.write(textSlice);
-        }
-        // process.stdin.write(this.prependCode.join('') + text);
-        process.stdin.end("\x04");
+        this.documentProcesses[uri] = process = childProcess.spawn(
+            this.perlExecutable,
+            ['-c', ...this.perlOptions, ...this.includePaths.map(path => '-I' + path)]
+        );
+
+        process.stdin.on('error', (err: Error) => {
+            if (diagnostics.length === 0) {
+                throw new Error(`No diagnostics were produced on perl exit: ${err.message}`);
+            }
+        });
+
+        process.addListener('exit', function(code: number, signal: string) {
+            callback(diagnostics);    
+        });   
+
         process.stderr.on('data', (lineBuf) => {
             const lineStr: string = lineBuf.toString();
             const lines: string[] = lineStr.split('\n');
+            let lastErrorLineNum = 0;
             lines.forEach((line, index) => {
                 if(line.match(LINE_REGEX)) {
                     let lineNum = this.extractLineNumber(line) - 1;
                     if(!isNaN(lineNum)) {
                         const diagnostic: Diagnostic = Diagnostic.create(
-                            Range.create(Position.create(lineNum, 0), Position.create(lineNum, line.length)),
+                            Range.create(
+                                Position.create(lineNum, 0),
+                                Position.create(lineNum, line.length)
+                            ),
                             line,
                             DiagnosticSeverity.Error
                         );
                         diagnostics.push(diagnostic);
+                        lastErrorLineNum = lineNum;
                     }
                 }    
+                if(line.match(/has too many errors\.$/)) {
+                    const diagnostic: Diagnostic = Diagnostic.create(
+                        Range.create(
+                            Position.create(lastErrorLineNum, 0),
+                            Position.create(lastErrorLineNum, line.length)
+                        ),
+                        line,
+                        DiagnosticSeverity.Error
+                    );
+                    diagnostics.push(diagnostic);
+                }
             });     
         });
-        process.addListener('exit', function(code: number, signal: string) {
-            callback(diagnostics);    
-        });   
+        
+        process.stdin.write(this.prependCode.join(''));
+        process.stdin.write(text);
+        process.stdin.end("\x04");
     }
 
     private extractLineNumber(line: string): number {
